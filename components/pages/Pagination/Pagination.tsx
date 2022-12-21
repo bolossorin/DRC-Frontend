e77bc57cd034b14e370ee0ab8e2fad2c18328adb
bolossorin <bolosvasilesorin@gmail.com>
2022-12-21 23:29:46 +0200

    fix: add Actions and Pagination

---
+++ components/pages/Pagination/Pagination.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Pagination = () => {
+
+  return (
+    <div className='flex items-center gap-6'>
+      <div className='font-medium'>
+        1 – 20 of 123
+      </div>
+      <div className='flex items-center gap-6'>
+        <div className='w-2 cursor-pointer transition-all opacity-50 hover:opacity-100'>
+          <img src='/arrow.svg' alt='' />
+        </div>
+        <div className='w-2 cursor-pointer transition-all opacity-50 hover:opacity-100 rotate-180'>
+          <img src='/arrow.svg' alt='' />
+        </div>
+      </div>
+    </div>
+  )
+}
