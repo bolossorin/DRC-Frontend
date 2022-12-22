201b4cb6e80ddb33651a5c7f0d5300f3683c59da
bolossorin <bolosvasilesorin@gmail.com>
2022-12-22 17:12:31 +0200

    feature: remove filter by ID

--- components/pages/vessels/Filters/Filters.tsx
+++ components/pages/vessels/Filters/Filters.tsx
@@ -6,13 +6,24 @@ interface IFilters {
 }
 
 export const Filters = ({ filters, setFilters }: IFilters) => {
+
   return (
     <div className='px-6 pb-6 pt-4'>
       {filters.length > 0 &&
         <div className='flex flex-wrap gap-4 items-center'>
           {filters.map((filter, index) =>
-            <div key={index} className='bg-[#3C3C3C] px-3 py-[9px] rounded border border-[#A4A4A4] text-sm'>
-              <span className='text-[#AAFF66]'>{filter.name}</span> : “{filter.value}“
+            <div key={index} className='flex bg-[#3C3C3C] px-3 rounded border border-[#A4A4A4] text-sm'>
+              <div className='flex items-center'>
+                <span className='py-[9px] text-[#AAFF66] mr-1'>{filter.name}</span> : “{filter.value}“
+              </div>
+              <div
+                onClick={() => {
+                  const newFilters = [...filters.slice(0, index), ...filters.slice(index + 1)]
+                  setFilters([...newFilters])
+                }}
+                className='flex items-center py-[9px] pl-3 ml-3 border-l border-[#A4A4A4] cursor-pointer group transition-all'>
+                <img className='w-2 opacity-50 group-hover:opacity-100' src={'/close.svg'} alt='' />
+              </div>
             </div>)}
           <button
             type='button'
