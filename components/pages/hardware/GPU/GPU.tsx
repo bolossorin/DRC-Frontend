54431819d969a0976675a26f1899938bf8507a06
Germandavid16 <davidthreeg86@gmail.com>
2023-01-05 15:17:49 -0800

    feature: create "Overall" section

--- components/pages/hardware/GPU/GPU.tsx
+++ components/pages/hardware/GPU/GPU.tsx
@@ -1,13 +1,17 @@
-import { H4 } from "../../../common";
 import React from "react";
+
+// components
+import { H4 } from "../../../common";
+import { Overall } from "../Overall/Overall";
 
 export const GPU = () => {
   return (
     <div className='py-6 px-3 sm:p-6 bg-[#2F2F2F] rounded'>
-      <H4 classname='flex items-center'>
+      <H4 classname='!mb-6 flex items-center'>
         <img className='w-7 mr-4' src={'/gpu.svg'} alt='' />
         GPU
       </H4>
+      <Overall />
     </div>
   )
 }
