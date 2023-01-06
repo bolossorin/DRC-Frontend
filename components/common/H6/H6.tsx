1de52dc55425d12a9f1ac54346832737ce9d829d
bolossorin <bolosvasilesorin@gmail.com>
2023-01-06 03:30:27 +0200

    feature: create "Usage per User" tab

--- components/common/H6/H6.tsx
+++ components/common/H6/H6.tsx
@@ -6,10 +6,13 @@ import cn from "classnames";
 interface IH6 {
   children: string
   classname?: string
+  onClick?: () => void
 }
 
-export const H6: FC<IH6> = ({ children, classname }) => {
+export const H6: FC<IH6> = ({ children, classname, onClick }) => {
   return (
-    <h6 className={cn('text-sm font-bold mb-2', classname)}>{children}</h6>
+    <h6 onClick={onClick} className={cn('text-sm font-bold mb-2', classname)}>
+      {children}
+    </h6>
   )
 }
