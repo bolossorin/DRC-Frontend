2f612ebbaa5e556824b91f36a3f9047d8ad56c77
Germandavid16 <davidthreeg86@gmail.com>
2023-01-10 14:07:49 -0800

    fix: title

--- pages/hardware.tsx
+++ pages/hardware.tsx
@@ -10,7 +10,7 @@ export default function Hardware() {
     <Layout
       title='Hardware | Deep Render Cloud'
       description='Hardware | Deep Render Cloud'
-      label={<H2 classname='mb-0'>Hardware</H2>}>
+      label={<H2 classname='!mb-0'>Hardware</H2>}>
       <div className='p-2 md:p-6 flex-1 flex flex-wrap justify-between gap-y-6'>
         <GPU />
         <GPU />
