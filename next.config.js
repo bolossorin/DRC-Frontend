c24fae1da285ec2b625f2803857afd09bfff4f9a
Germandavid16 <davidthreeg86@gmail.com>
2023-02-13 09:15:17 -0800

    fix: apollo get token url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    LOCAL_SERVER_URL: process.env.LOCAL_SERVER_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
