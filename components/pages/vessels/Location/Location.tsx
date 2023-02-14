b761ee18d79221d6a61f94a57c76d3169695f1c8
Germandavid16 <davidthreeg86@gmail.com>
2023-02-14 07:06:49 -0800

    feature: regions graphql

--- components/pages/vessels/Location/Location.tsx
+++ components/pages/vessels/Location/Location.tsx
@@ -1,23 +1,25 @@
 // libs
-import Select from 'react-select';
+import { useQuery } from "@apollo/client";
+import Select from "react-select";
+import { getRegions } from "../../../../graphql/regions/getRegions";
 
 // components
-import { DropdownIndicator } from '../../../common'
-
-const locations: ({ label: string; value: string })[] = [
-  { value: 'Europe', label: 'Europe' },
-  { value: 'USA', label: 'USA' },
-];
+import { DropdownIndicator } from "../../../common";
 
 export const Location = () => {
+  const { data } = useQuery<{ available_regions: string[] }>(getRegions);
+
+  const options =
+    data?.available_regions?.map((region) => ({ value: region, label: region })) ?? [];
+
   return (
     <Select
       // @ts-ignore
       components={{ DropdownIndicator }}
       className="basic-single location"
       classNamePrefix="select"
-      defaultValue={locations[0]}
-      options={locations}
+      defaultValue={options?.[0]}
+      options={options}
     />
-  )
-}
+  );
+};
