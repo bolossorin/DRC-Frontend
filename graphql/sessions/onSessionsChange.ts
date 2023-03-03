afa44177b3df5a2523f298943078a48ccff87f70
Germandavid16 <davidthreeg86@gmail.com>
2023-03-03 08:19:30 -0800

    feature: sessions subscription

---
+++ graphql/sessions/onSessionsChange.ts
@@ -0,0 +1,29 @@
+import { gql } from "@apollo/client";
+
+export const onSessionsChange = gql`
+  subscription onSessionsChange($region: String) {
+    my_sessions(region: $region) {
+      id
+      fqdn
+      container_id
+      region
+      vm_hostname
+      user_id
+      label
+      tailscale_ip
+      queue
+      vm_hostname
+      vm_id
+      n_gpus
+      avg_gpu_util
+      avg_gpu_memory_util
+      image
+      name
+      gpu_ids
+      state
+      error_message
+      created_at
+      modified_at
+    }
+  }
+`;
