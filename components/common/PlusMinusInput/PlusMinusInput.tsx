84437697de33ece6a7984afa864af85991689d6f
bolossorin <bolosvasilesorin@gmail.com>
2023-03-21 23:41:14 +1000

    Fix bugs with the creation of vessels
    1. Fix the original number of input fields for names.
    2. Fix processing of some fields.
    3. Add minimum values.
    4. Fix the behavior of the list of input fields when the quantity does not change.

--- components/common/PlusMinusInput/PlusMinusInput.tsx
+++ components/common/PlusMinusInput/PlusMinusInput.tsx
@@ -1,20 +1,24 @@
 interface IPlusMinusInput {
   value: number
   setValue: (value: number) => void
+  minValue?: number
+  maxValue?: number
 }
 
-export const PlusMinusInput = ({ value, setValue }: IPlusMinusInput) => {
+export const PlusMinusInput = ({ value, setValue, minValue, maxValue }: IPlusMinusInput) => {
+  const min = minValue !== undefined ? minValue : 0
+  const max = maxValue !== undefined ? maxValue : 9999999
 
   return (
     <div className='flex'>
       <div
-        onClick={() => setValue(value > 0 ? value - 1 : 0)}
+        onClick={() => setValue(value > min ? value - 1 : min)}
         className='flex items-center justify-center w-8 bg-[#474747] rounded-sm p-2.5 cursor-pointer transition-all hover:bg-[#686868] select-none'>
         <img src={'./minus.svg'} alt='' />
       </div>
       <input className='w-8 text-center bg-transparent select-none' type='text' disabled value={value} />
       <div
-        onClick={() => setValue(value + 1)}
+        onClick={() => setValue(value < max ? value + 1 : max)}
         className='flex items-center justify-center w-8 bg-[#474747] rounded-sm p-2.5 cursor-pointer transition-all hover:bg-[#686868] select-none'>
         <img src={'./plus.svg'} alt='' />
       </div>
