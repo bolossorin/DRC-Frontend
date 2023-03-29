70e1149ae9aea9e436d1c2da98ea95468984b4b9
bolossorin <bolosvasilesorin@gmail.com>
2023-03-29 18:56:46 +0500

    Wrap the number of vessels in brackets

--- components/common/VesselTitle/VesselTitle.tsx
+++ components/common/VesselTitle/VesselTitle.tsx
@@ -9,7 +9,7 @@ export const VesselTitle = ({ count }: { count?: number }) => {
     <>
       <H2 classname="!mb-0">Vessels</H2>
       {count !== undefined && (
-        <Paragraph classname="text-lg md:text-xl ml-2 md:ml-4 !mb-0 relative top-0.5">{count}</Paragraph>
+        <Paragraph classname="text-lg md:text-xl ml-2 md:ml-4 !mb-0 relative top-0.5">({count})</Paragraph>
       )}
     </>
   );
