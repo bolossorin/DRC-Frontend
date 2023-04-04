7fe3850e4d4272ea3c276f9d771873fbf45cde26
Germandavid16 <davidthreeg86@gmail.com>
2023-04-04 11:14:59 -0700

    fix: table cell width

--- components/pages/vessels/Table/Table.tsx
+++ components/pages/vessels/Table/Table.tsx
@@ -91,74 +91,78 @@ export const Table = (
         />
       )}
       <table className={className}>
-        <Row classname={styles.row}>
-          <Cell classname="w-12">
-            <img className="opacity-50 w-4 h-4" src="/dots.svg" alt="" />
-          </Cell>
-          <Cell classname="w-[40px] relative top-0.5">
-            <Checkbox onChange={() => setSelectAll(!selectAll)} checked={selectAll} />
-          </Cell>
-          {columns.map((header) => (
-            <Cell classname={header.label.toLowerCase().replaceAll(' ', '')} key={header.key}>
-              {header.label}
+        <thead>
+          <Row classname={styles.row}>
+            <Cell classname="w-12">
+              <img className="opacity-50 w-4 h-4" src="/dots.svg" alt="" />
             </Cell>
-          ))}
-        </Row>
-        {items ? items.map((row, index) => (
-            <Row key={index}
-                 classname={cn({ "!bg-[#3A3A3A]": isSelected(row.id) }, styles.row, { [styles.animation]: (row.state === 'starting' || row.state === 'requested') })}>
-              <Cell classname="!w-12 cursor-pointer relative overflow-visible group">
-                <img className="opacity-50 group-hover:opacity-100 transition-all w-4 h-4" src="/dots.svg" alt="" />
-                <ul
-                  className={cn(
-                    "hidden group-hover:block w-max absolute z-20 top-4 left-4 rounded border border-[#686868] bg-[#3D3C3C]",
-                    listStyles.list,
-                    listStyles.small
-                  )}
-                >
-                  <StopButton
-                    onClick={() => handleOpenStopVesselModal(row.id)}
-                    disabled={inactiveSessionStatuses.includes(row.state)} />
-                  <li
+            <Cell classname="w-[40px] relative top-0.5">
+              <Checkbox onChange={() => setSelectAll(!selectAll)} checked={selectAll} />
+            </Cell>
+            {columns.map((header) => (
+              <Cell classname={header.label.toLowerCase().replaceAll(' ', '')} key={header.key}>
+                {header.label}
+              </Cell>
+            ))}
+          </Row>
+        </thead>
+        <tbody>
+          {items ? items.map((row, index) => (
+              <Row key={index}
+                  classname={cn({ "!bg-[#3A3A3A]": isSelected(row.id) }, styles.row, { [styles.animation]: (row.state === 'starting' || row.state === 'requested') })}>
+                <Cell classname="!w-12 cursor-pointer relative overflow-visible group">
+                  <img className="opacity-50 group-hover:opacity-100 transition-all w-4 h-4" src="/dots.svg" alt="" />
+                  <ul
                     className={cn(
-                      "flex items-center border-b border-b-[#686868] hover:bg-[#535353] transition-all cursor-pointer select-none",
-                      inactiveSessionStatuses.includes(row.state) && "opacity-50 cursor-default hover:bg-inherit"
+                      "hidden group-hover:block w-max absolute z-20 top-4 left-4 rounded border border-[#686868] bg-[#3D3C3C]",
+                      listStyles.list,
+                      listStyles.small
                     )}
                   >
-                    {!inactiveSessionStatuses.includes(row.state) ? (
-                      <Link href={"https://" + row?.fqdn} target="_blank" className="flex items-center">
-                        <img className="w-4 mr-3" src="/vs-code.svg" alt="" />
-                        <p>VS Code</p>
-                      </Link>
-                    ) : (
-                      <>
-                        <img className="w-4 mr-3" src="/vs-code.svg" alt="" />
-                        <p>VS Code</p>
-                      </>
-                    )}
-                  </li>
-                  <CopyButton content={row.ssh_config} label="Copy SSH Config" />
-                  <CopyButton content={row.ssh_command} label="Copy SSH Command" />
-                </ul>
+                    <StopButton
+                      onClick={() => handleOpenStopVesselModal(row.id)}
+                      disabled={inactiveSessionStatuses.includes(row.state)} />
+                    <li
+                      className={cn(
+                        "flex items-center border-b border-b-[#686868] hover:bg-[#535353] transition-all cursor-pointer select-none",
+                        inactiveSessionStatuses.includes(row.state) && "opacity-50 cursor-default hover:bg-inherit"
+                      )}
+                    >
+                      {!inactiveSessionStatuses.includes(row.state) ? (
+                        <Link href={"https://" + row?.fqdn} target="_blank" className="flex items-center">
+                          <img className="w-4 mr-3" src="/vs-code.svg" alt="" />
+                          <p>VS Code</p>
+                        </Link>
+                      ) : (
+                        <>
+                          <img className="w-4 mr-3" src="/vs-code.svg" alt="" />
+                          <p>VS Code</p>
+                        </>
+                      )}
+                    </li>
+                    <CopyButton content={row.ssh_config} label="Copy SSH Config" />
+                    <CopyButton content={row.ssh_command} label="Copy SSH Command" />
+                  </ul>
+                </Cell>
+                <Cell classname="w-[40px] relative top-0.5">
+                  <Checkbox onChange={handleSelect({ id: row.id, state: row.state })} checked={isSelected(row.id)} />
+                </Cell>
+                {columns.map(({ renderCell, key, label }) =>
+                  renderCell ? renderCell(row, key)
+                    : <Cell
+                      classname={label.toLowerCase().replaceAll(' ', '')}
+                      key={key}>
+                      {row[key as keyof ISession]}
+                    </Cell>
+                )}
+              </Row>
+            ))
+            : <Row>
+              <Cell colspan={columns.length + 2}>
+                <H4 classname="w-full text-center py-4">Loading...</H4>
               </Cell>
-              <Cell classname="w-[40px] relative top-0.5">
-                <Checkbox onChange={handleSelect({ id: row.id, state: row.state })} checked={isSelected(row.id)} />
-              </Cell>
-              {columns.map(({ renderCell, key }) =>
-                renderCell ? renderCell(row, key)
-                  : <Cell
-                    classname={row.label.toLowerCase().replaceAll(' ', '')}
-                    key={key}>
-                    {row[key as keyof ISession]}
-                  </Cell>
-              )}
-            </Row>
-          ))
-          : <Row>
-            <Cell colspan={columns.length + 2}>
-              <H4 classname="w-full text-center py-4">Loading...</H4>
-            </Cell>
-          </Row>}
+            </Row>}
+          </tbody>
       </table>
     </>
   );
