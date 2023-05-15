95f16d6947f4f114a8a11e58446e5d0f4ccbd575
Germandavid16 <davidthreeg86@gmail.com>
2023-05-15 11:18:31 -0700

    added mock charts

--- components/pages/vessel-id/index.tsx
+++ components/pages/vessel-id/index.tsx
@@ -1,4 +1,5 @@
-export { Information } from './Information/Information'
-export { Connection } from './Connection/Connection'
-export { Experiments } from './Experiments/Experiments'
-export { TableExperiments } from './TableExperiments/TableExperiments'
+export { Information } from "./Information/Information";
+export { Connection } from "./Connection/Connection";
+export { Experiments } from "./Experiments/Experiments";
+export { TableExperiments } from "./TableExperiments/TableExperiments";
+export { Monitoring } from "./Monitoring/Monitoring";
